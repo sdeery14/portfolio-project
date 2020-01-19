68290cd4769f8551b0ed2010b39fc882e20db072
Sean <sdeery14@comcast.net>
2020-01-19 17:45:13 -0500

    added API

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from django.shortcuts import render
 from joblib import load
+import requests
 
 from .forms import TitanicForm
 
@@ -16,19 +17,18 @@ def titanic(request):
 	if request.method == 'POST':
 		titanic_form = TitanicForm(request.POST)
 		if titanic_form.is_valid():
-			pclass = titanic_form.cleaned_data['pclass']
-			age = titanic_form.cleaned_data['age']
-			sibsp = titanic_form.cleaned_data['sibsp']
-			parch = titanic_form.cleaned_data['parch']
-			fare = titanic_form.cleaned_data['fare']
-			sex = titanic_form.cleaned_data['sex']
-			embarked = titanic_form.cleaned_data['embarked']
 
-			X_test = pd.DataFrame(np.array([pclass, age, sibsp, parch, fare, sex, embarked]).reshape(1,-1), 
-				columns=['Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Sex', 'Embarked'])
+			X_test = {
+				"Pclass": titanic_form.cleaned_data['pclass'],
+				"Age": titanic_form.cleaned_data['age'],
+				"Sibp": titanic_form.cleaned_data['sibsp'],
+				"Parch": titanic_form.cleaned_data['parch'],
+				"Fare": titanic_form.cleaned_data['fare'],
+				"Sex": titanic_form.cleaned_data['sex'],
+				"Embarked": titanic_form.cleaned_data['embarked']
+			}
 
-			predictor = load("predictor.joblib")
-			prediction = "Survived: Yes" if predictor.predict(X_test) == 1 else "Survived: No"
+			prediction = requests.post("https://seandeery-api.com/titanic_prediction/", data = X_test).json()
 
 			return render(request, 'titanic.html', {'titanic_form': titanic_form, 'prediction': prediction})
 	else:
